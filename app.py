822dea1bc430b2b1f7814381b56fd76d509abd93
nveeee <whatshouldprowerplay@gmail.com>
2025-03-19 18:41:14 -0400

    Add order route and some comments

--- app.py
+++ app.py
@@ -2,10 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Define Shipping class
+
+# Define Payment class
+
+# Define Order class
+# An order is expected to have:
+# one or many items, shipping information, payment information
+
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+@app.route('/order', methods=['POST'])
+def place_order():
+    return 'Order'
 
 
 if __name__ == '__main__':
