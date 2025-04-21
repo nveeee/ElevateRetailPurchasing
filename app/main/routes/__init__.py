782f54a2a4863f9e9c867d29dabec925eb404ac1
nveeee <whatshouldprowerplay@gmail.com>
2025-04-21 18:51:52 -0400

    Remove redundant code comment. Rename success page title.

--- app/main/routes/__init__.py
+++ app/main/routes/__init__.py
@@ -1,13 +1,5 @@
-#rom flask import Blueprint
-
-#bp = Blueprint('main', __name__)
-
-#from . import index, po_form, place_order, order_success  # noqa: F401, E402
-
 from flask import Blueprint
 
-#Define the Blueprint for the main routes
 bp = Blueprint('main', __name__)
 
-#Import your route handlers here
 from . import index, po_form, place_order, order_success, order_stages  # noqa: F401, E402
