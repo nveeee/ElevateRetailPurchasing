bd4a6dc3cdfdfcdacb35ac8c24b9f49d7abae3c7
max <maxhernandez04@gmail.com>
2025-04-21 18:45:15 -0400

    maxx

--- app/main/routes/__init__.py
+++ app/main/routes/__init__.py
@@ -1,6 +1,13 @@
+#rom flask import Blueprint
+
+#bp = Blueprint('main', __name__)
+
+#from . import index, po_form, place_order, order_success  # noqa: F401, E402
+
 from flask import Blueprint
 
+#Define the Blueprint for the main routes
 bp = Blueprint('main', __name__)
 
-from . import index, po_form, place_order, order_success  # noqa: F401, E402
-
+#Import your route handlers here
+from . import index, po_form, place_order, order_success, order_stages  # noqa: F401, E402
