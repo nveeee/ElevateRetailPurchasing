0a115d759f4886bc61446b9fb790f96004596a19
max <maxhernandez04@gmail.com>
2025-04-22 06:35:36 -0400

    maxx

--- app/main/routes/order_stages.py
+++ app/main/routes/order_stages.py
@@ -4,27 +4,28 @@ from ...main import bp
 from ...schemas.purchase_order import PurchaseOrder
 from ...database import db
 
-# Route to display order status page
+# Route to display order statuses
 @bp.route('/order_stages', methods=['GET'])
 def order_stages():
-    orders = PurchaseOrder.query.all()
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
 
-    if not orders:
-        return render_template(
-            'order_stages.html',
-            orders=None,
-            message="There are no orders in place right now.",
-            env=os.getenv('FLASK_ENV')
-        )
+    #orders using Flask-SQLAlchemy
+    orders_query = PurchaseOrder.query.paginate(page=page, per_page=per_page, error_out=False)
+    orders = orders_query.items
+    total_pages = orders_query.pages
 
     return render_template(
         'order_stages.html',
         orders=orders,
         message=None,
+        current_page=page,
+        total_pages=total_pages,
+        active_page='order_stages',
         env=os.getenv('FLASK_ENV')
     )
 
-# Route to update status of an order
+# Route to update the status of an order
 @bp.route('/update_order_status/<int:order_id>', methods=['POST'])
 def update_order_status(order_id):
     order = PurchaseOrder.query.get(order_id)
@@ -33,9 +34,9 @@ def update_order_status(order_id):
         return redirect(url_for('main.order_stages'))
 
     new_status = request.form.get('status')
-
     if new_status:
         order.status = new_status
         db.session.commit()
 
     return redirect(url_for('main.order_stages'))
+
